310aea788d13ef01c5d7ffcf21ad9455b19891a1
gWasik <70466408+gWasik@users.noreply.github.com>
2025-12-06 22:27:26 +0300

    Update feed URL for passwall packages

--- passwall2x.sh
+++ passwall2x.sh
@@ -64,7 +64,7 @@ read release arch << EOF
 $(. /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
 EOF
 for feed in passwall_luci passwall_packages passwall2; do
-  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
+  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release.02/$arch/$feed" >> /etc/opkg/customfeeds.conf
 done
 
 ### Install package ###
